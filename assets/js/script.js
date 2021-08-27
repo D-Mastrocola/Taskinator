09a69c0c70f42765575ef2d41e0832690346ab11
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-08-27 11:39:33 -0400

    takes user input and adds it to tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,28 @@
-var formEl = document.querySelector("#task-form");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+let formEl = document.querySelector("#task-form");
+let tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
-
+let createTaskHandler = function (event) {
   //Prevents the browser from reloding on form submit
   event.preventDefault();
 
-  var listItemEl = document.createElement("li");
+  let taskNameInput = document.querySelector("input[name='task-name']").value;
+  let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  let listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameInput +
+    "</h3><span class='task-type'>" +
+    taskTypeInput +
+    "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
   tasksToDoEl.appendChild(listItemEl);
 };
 
